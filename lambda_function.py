f4c2fac52123f52ee19e5c95d0b41cfe4ba2163f
GeonPark <smsh0807@gmail.com>
2023-12-13 20:58:02 +0900

    랜덤 추가

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,6 @@
 import json
 import pymysql
+import random
 
 def lambda_handler(event, context):
     # AWS Lex에서 전달된 증상 가져오기
@@ -15,28 +16,29 @@ def lambda_handler(event, context):
     response_content = ''
     try:
         with connection.cursor() as cursor:
-            sql = "SELECT itemName, seQesitm, depositMethodQesitm, efcyQesitm, useMethodQesitm FROM medidat WHERE efcyQesitm LIKE %s LIMIT 1"
+            # SQL 쿼리 수정: 랜덤 결과 가져오기 및 NULL이 아닌 결과만 선택
+            sql = """
+            SELECT itemName, seQesitm, depositMethodQesitm, efcyQesitm, useMethodQesitm 
+            FROM medidat 
+            WHERE efcyQesitm LIKE %s AND itemName IS NOT NULL AND seQesitm IS NOT NULL 
+            AND depositMethodQesitm IS NOT NULL AND efcyQesitm IS NOT NULL AND useMethodQesitm IS NOT NULL 
+            ORDER BY RAND() LIMIT 1
+            """
             cursor.execute(sql, ('%' + symptom + '%',))
 
-            results = cursor.fetchall()
+            results = cursor.fetchone()
 
-
-            for row in results:
-                item_name = row[0] if row[0] else "정보 없음"
-                side_effect = row[1] if row[1] else "정보 없음"
-                storage_method = row[2] if row[2] else "정보 없음"
-                effect = row[3] if row[3] else "정보 없음"
-                use_method = row[4] if row[4] else "정보 없음"
+            if results:
+                item_name, side_effect, storage_method, effect, use_method = results
                 
-                insert_sql = f"INSERT INTO request (symptom, item_name, side_effect, storage_method, effect, use_method) VALUES (%(symptom)s, %(item_name)s, %(side_effect)s, %(storage_method)s, %(effect)s, %(use_method)s)"
-                cursor.execute(insert_sql, {'symptom': symptom, 'item_name': item_name, 'side_effect': side_effect, 'storage_method': storage_method, 'effect': effect, 'use_method': use_method})
+                insert_sql = """
+                INSERT INTO request (symptom, item_name, side_effect, storage_method, effect, use_method) 
+                VALUES (%s, %s, %s, %s, %s, %s)
+                """
+                cursor.execute(insert_sql, (symptom, item_name, side_effect, storage_method, effect, use_method))
                 connection.commit()
                 
-                response_content += '약 이름: ' + item_name + ', '
-                response_content += '부작용: ' + side_effect + ', '
-                response_content += '보관 방법: ' + storage_method + ', '
-                response_content += '효과: ' + effect + ', '
-                response_content += '사용 방법: ' + use_method + ', '
+                response_content += f'약 이름: {item_name}, 부작용: {side_effect}, 보관 방법: {storage_method}, 효과: {effect}, 사용 방법: {use_method}'
 
     finally:
         connection.close()
